6745d979f3dd755361dd23f61746dbc919db550c
Maricla Kandzorra Martelli <maricla.kandzorra8@gmail.com>
2022-05-20 18:37:44 +0200

    date-time-range calendar first setup

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,28 @@
 import Image from 'next/image'
 import { GlobeAltIcon, MenuIcon, SearchIcon, UserCircleIcon } from '@heroicons/react/solid'
-
+import { useState} from 'react'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
+    const [searchInput, setSearchInput] = useState('');
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+
+    const handleSelect= (ranges) => {
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    };
+    
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'Selection',
+    };
+
+    
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 
          bg-white shadow-md p-5 md:px-10">
@@ -23,7 +43,10 @@ function Header() {
 
             {/*Middle - Search box heroicons.com npm install @heroicons/react*/}
             <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-                <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-500' type='text' placeholder='Start your search'/>
+                <input 
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-500' type='text' placeholder='Start your search'/>
                 <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 md:mx-2 shadow-md hover:shadow-xl active:scale-90 duration-150' />
             </div>
 
@@ -38,7 +61,17 @@ function Header() {
                 <UserCircleIcon className='h-5' />
                 </div>
             </div>
-             
+
+            {searchInput && (
+            <div className='flex flex-col col-span-3 mx-auto'>
+                <DateRangePicker ranges={[selectionRange]} 
+                minDate={new Date()}
+                rangeColors={["#FD5B61"]}
+                onChange={handleSelect}
+                />                
+            
+            </div>
+            )}
         </header>
     );
 }
