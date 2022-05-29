ca4ffa79387d6ddc50bdc68d5efc73dc1eacba36
Maricla Kandzorra Martelli <maricla.kandzorra8@gmail.com>
2022-05-29 13:43:30 +0200

    adjusting vercel errors3

--- pages/search.js
+++ pages/search.js
@@ -1,3 +1,5 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Search() {
     return (
@@ -6,7 +8,7 @@ function Search() {
 			
 			<main className='flex'>
 				<section className='flex-grow pt-14 px-5'>
-					<p className='text-xs'>300plus Venues for 500 Event Visitors</p>
+					<p className='text-xs'>300+ Venues for 500 Event Visitors</p>
 					<h1 className='text-3xl font-semibold mt-2 mb-6'>Stay in Adventurous Castle on the Island</h1>
 					<tr className='hidden md:inline-flex lg:inline-flex m-8 mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
 						<td>
@@ -25,7 +27,7 @@ function Search() {
 						<p className='searchsitemenubutton'>Number of Visitors</p>
 						</td>
 						<td>
-						<p className='searchsitemenubutton'>Customer's review</p>
+						<p className='searchsitemenubutton'>Customer&apos;s review</p>
 						</td>
 						<td>
 						<p className='searchsitemenubutton'>More Filters</p>
@@ -40,7 +42,7 @@ function Search() {
 			
 			<Footer />
         </div>
-    );
+    )
 }
 
 export default Search; 
