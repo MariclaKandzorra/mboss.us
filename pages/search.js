8faa8a8b4efc56c1ed03b901f2df92f9c016d99b
Maricla Kandzorra Martelli <maricla.kandzorra8@gmail.com>
2022-05-29 13:10:04 +0200

    adjusting vercel errors1

--- pages/search.js
+++ pages/search.js
@@ -8,7 +8,7 @@ function Search() {
 			
 			<main className='flex'>
 				<section className='flex-grow pt-14 px-5'>
-					<p className='text-xs'>300+ Venues for 500 Event Visitors</p>
+					<p className='text-xs'>300plus Venues for 500 Event Visitors</p>
 					<h1 className='text-3xl font-semibold mt-2 mb-6'>Stay in Adventurous Castle on the Island</h1>
 					<tr className='hidden md:inline-flex lg:inline-flex m-8 mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
 						<td>
@@ -42,7 +42,7 @@ function Search() {
 			
 			<Footer />
         </div>
-    )
+    );
 }
 
 export default Search; 
