2d296e8d2eb65684f05151e889e10bab035abd27
Maricla Kandzorra Martelli <maricla.kandzorra8@gmail.com>
2022-06-03 00:24:21 +0200

    formatted date/time for search.js

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,27 @@
+import { useRouter } from 'next/dist/client/router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { format, compareAsc } from 'date-fns';
 
 function Search() {
+const router = useRouter();
+
+const{ location, startDate, endDate, time1, time2, noOfGuests } = router.query;
+
+const formStartDate = format(new Date(startDate), 'dd.MM.yyyy');
+const formEndDate = format(new Date(endDate), 'dd.MM.yyyy');
+const formTime1 = format(new Date(time1), 'HH:mm:ss');
+const formTime2 = format(new Date(time2), 'HH:mm:ss');
+
+
     return (
         <div>
             <Header />
 			
 			<main className='flex'>
 				<section className='flex-grow pt-14 px-5'>
-					<p className='text-xs'>300+ Venues for 500 Event Visitors</p>
-					<h1 className='text-3xl font-semibold mt-2 mb-6'>Stay in Adventurous Castle on the Island</h1>
+					<p className='text-xs'>300+ Venues from {formStartDate} at {formTime1} to {formEndDate} at {formTime2} for {noOfGuests} Event Visitors</p>
+					<h1 className='text-3xl font-semibold mt-2 mb-6'>Stay in {location}</h1>
 					<tr className='hidden md:inline-flex lg:inline-flex m-8 mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
 						<td>
 						<p className='searchsitemenubutton'>Cancellation Flexibility</p>
