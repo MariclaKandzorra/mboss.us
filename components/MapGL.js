2927fea7f0d981ce662cf3cc5be291e61927270a
Maricla Kandzorra Martelli <maricla.kandzorra8@gmail.com>
2022-06-20 11:28:05 +0200

    Arcgis Hybrid adjusted4

--- components/MapGL.js
+++ components/MapGL.js
@@ -9,7 +9,7 @@ import Search from '@arcgis/core/widgets/Search';
 import Home from '@arcgis/core/widgets/Home';
 import Legend from '@arcgis/core/widgets/Legend';
 import Expand from '@arcgis/core/widgets/Expand';
-import Config from '@arcgis/core/core/config';
+import Config from '@arcgis/core/config';
 
 function MapGL({ Map, GeoJSONLayer, MapView, FeatureLayer, Directions, Search, Home, Legend, Expand, Config }) {
             // If GeoJSON files are not on the same domain as your website, a CORS enabled server
